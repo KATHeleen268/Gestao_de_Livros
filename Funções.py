8b1bf4a54774f885352c5d52fc9a9456e05700ea
Katheleen Eduarda <katheleeneduarda123@gmail.com>
2025-04-11 18:50:12 -0300

    git commit -m docs: documentando o código

--- Funções.py
+++ Funções.py
@@ -5,7 +5,7 @@ from funçõescsv import salvar_livros
 
 #função de inserir livros que recebe os dados do usario e depois adicona e salva na biblioteca
 def inserir_livros(biblioteca):
-    id = int(input("Digite o código do livro: ")) #pede o id ao usuario e o receb como inteiro 
+    id = int(input("Digite o código do livro: ")) #pede o id ao usuario e o recebe como inteiro 
     titulo = input("Digite o titulo do livro: ")
     autores = input("Digite o nome do(s) autor(a) do livro: ")
     editora = input("digite o nome da editora do livro: ")
@@ -27,64 +27,70 @@ def inserir_livros(biblioteca):
 
 #função remover o livro, que remove um livro da lista, tendo como chave o titulo
 def remover_livros(biblioteca):
-        titulo_remover = input("Digite o titulo do livro que você deseja remover: ") #pedde o titulo ao usuario par poder comparar e imprimir
+        titulo_remover = input("Digite o titulo do livro que você deseja remover: ") #pede o titulo ao usuario par poder comparar e imprimir
 
+    #quando o livro for enoocntrado irá remove-lo da lista e salvar no csv
         for livro in biblioteca:
             if livro["titulo"] == titulo_remover:
                 biblioteca.remove(livro)
                 salvar_livros(biblioteca)
                 print(f"Livro com titulo {titulo_remover} removido com sucesso!")
                 return
+            #caso não encontre avisa que o livro não existe
         print(f"Nenhum livro com o titulo {titulo_remover} foi encontrado.")
 
 
 #função que checa a situação do livro
 def situacao_livro(biblioteca):
-    titulo_situacao = input("Digite o titulo do livro que deseja ver a situação: ").strip().lower()
-    encontrado = False
+    titulo_situacao = input("Digite o titulo do livro que deseja ver a situação: ").strip().lower() #irá pedir o valor ao usuario e assim os metodos stip que converte tudo para mnusculas
+
+    encontrado = False #flag que auxilia no resultado da busca
 
     for livro in biblioteca:
-        if titulo_situacao in livro["titulo"].strip().lower():
-            print(f"O livro {livro['titulo']} está {livro['situação']}")
+        if titulo_situacao in livro["titulo"].strip().lower(): #compara o titulo que o usuario digitou com o que está na biblioteca se for existente irá mostrar sua situação "Disponível" ou "Emprestado"
+            print(f"O livro {livro['titulo']} está {livro['situação']}") 
             encontrado = True
             break
-
-    if not encontrado:
+             
+             #caso não encontre essa mensagem será exibida
+    if not encontrado: 
         print("Livro não existe na biblioteca.")
 
 #função de alugar livros ou seja troca a situação de disponivel para emprestado, caso não esteja disponivel
 def alugar_livros(biblioteca):
-    id_aluguel = int(input("Digite o id do livro que deseja alugar: "))
+    id_aluguel = int(input("Digite o id do livro que deseja alugar: ")) #pede o id do livro que o usuario deseja alugar 
     
     encontrado = False
 
+# irá procurar o livro e caso encontre só permite alugar o livro se estiver disponivel 
     for livro in biblioteca:
         if livro['id'] == id_aluguel:
             print(f"O livro '{livro['titulo']}' (ID : {livro['id']}) foi encontrado")
             encontrado = True
-            if livro["situação"] == "Disponível":
+            if livro["situação"] == "Disponível": #se estiver disponivel, fica emprestado
                 livro["situação"] = "Emprestado"
                 salvar_livros(biblioteca)
                 print(f"O livro '{livro['titulo']}' foi emprestado com sucesso!")
             else:
-                print(f"O livro '{livro['titulo']}' já está emprestado.")
+                print(f"O livro '{livro['titulo']}' já está emprestado.") #caso esteja emprestado já, ele informa
             break
     if not encontrado:
         print("O livro desejado não foi encontrado.")
 
-#função de devolução de livros
+#função de devolução de livros devolve um livro, mudando sua situação de "Emprestado" para "Disponível"
 def devolver_livros(biblioteca):
-    id_devolucao = int(input("Digite o ID do livro que deseja devolver: "))
+    id_devolucao = int(input("Digite o ID do livro que deseja devolver: ")) #pede o id do livro desejado ao usuario
 
-    encontrado = False
+    encontrado = False #flag
 
     for livro in biblioteca:
         if livro['id'] == id_devolucao:
             encontrado = True
-            if livro["situação"] == "Emprestado":
+            if livro["situação"] == "Emprestado": #se estiver como emprestado será mudado para emprestado
                 livro["situação"] = "Disponível"
                 print(f"O livro '{livro['titulo']}' foi devolvido com sucesso.")
-                salvar_livros(biblioteca)
+                salvar_livros(biblioteca) #chama a função de salvar livro 
+
         else:
             print(f"O livro '{livro['titulo']}' já está disponivel na biblioteca")
         break
@@ -95,7 +101,7 @@ def devolver_livros(biblioteca):
      
 #função de listar os livros de formas geral e especificas
 def listar_livros(biblioteca):
-    while True:
+    while True: #loop do submenu que só será encerrado quando o usuário escolher a opção de voltar ao menu inicial
         print("1. Listar todos os livros")
         print("2. Listar apenas os livros disponíveis")
         print("3. Listar apenas os livros emprestados")
@@ -103,30 +109,30 @@ def listar_livros(biblioteca):
         print("5. Listar por ano")
         print("6. Voltar ao menu principal")
         
-        opcao1 = input("Qual a listagem que deseja? ")
+        opcao1 = input("Qual a listagem que deseja? ") #o sistema pede ao usuario para digitar qual opcao de listagem ele deseja que seja executada
         
-        if opcao1 == "1":
+        if opcao1 == "1": #listagem geral de todos os livros da biblioteca
             for livro in biblioteca:
                 print(livro)
-        elif opcao1 == "2":
+        elif opcao1 == "2": #lista os livros disponiveis
             for livro in biblioteca:
                 if livro['situação'] == "Disponível":
                     print(livro)
-        elif opcao1 == "3":
+        elif opcao1 == "3": #lista os livros emprestados
             for livro in biblioteca:
                 if livro["situação"] == "Emprestado": 
                     print(livro)
-        elif opcao1 == "4":
+        elif opcao1 == "4": #lista os livros por nome de editora
             editora = input("Digite o nome da editora: ").strip().lower()
             for livro in biblioteca:
                 if livro["editora"].strip().lower() == editora:
                     print(livro)
-        elif opcao1 == "5":
+        elif opcao1 == "5": #lista os livros por ano 
             ano = int(input("Digite o ano que deseja: "))
             for livro in biblioteca:
                 if livro["ano"] == ano:
                     print(livro)
-        elif opcao1 == "6":
+        elif opcao1 == "6": #retorna ao menu principal
             print("retornando ao menu principal")
             break
         else:
@@ -134,15 +140,15 @@ def listar_livros(biblioteca):
 
 #função de buscar_livros que permite encontrar um livro pelo id
 def buscar_livros(biblioteca):
-    try:
-        id_busca = int(input("Digite o id do livro desejado: ")) #pedindo ao ussuario o id desejado
+    try: 
+        id_busca = int(input("Digite o id do livro desejado: ")) #pedindo ao usuario o id desejado 
     except ValueError:
         print("ID inválido! Digite apenas números.")
         return
 
     encontrado = False #flag usada para saber se o livro foi encontrado ou não
 
-    for livro in biblioteca:
+    for livro in biblioteca: # se encontrar o livro irá exibir se não irá informar que não existe
         if livro ['id'] == id_busca:
                 print(livro)
                 encontrado = True
--- funçõescsv.py
+++ funçõescsv.py
@@ -1,32 +1,41 @@
+#Importa o módulo CSv para leitura e escrita em arquivos.csv
 import csv
 
 ARQUIVO_CSV = "biblioteca.csv" #nome do  arquivo, váriavel fixa, ou seja uma constante
 
-#função carregar livros
+#função carregar livros do arquivo  csv e retornar como uma lista de dicionario
 def carregar_livros():
-    biblioteca = []
+    biblioteca = [] #inicaliza a lista que armazena os livros carregados
+
     try:
+        #abre os arquivos csv para leitura
         with open(ARQUIVO_CSV, newline='', encoding='utf-8') as arquivo_livros:
              leitor_csv = csv.DictReader(arquivo_livros)
              for livros_csv in leitor_csv:
+                 #converte os arquivos do csv para o formato correto e armazena em um novo dicionario
                  livro = {
-                     "id": int(livros_csv["id"]),
+                     "id": int(livros_csv["id"]), #converte o valor para inteiro
                      "titulo": livros_csv["titulo"],
                      "autores": livros_csv["autores"],
                      "editora": livros_csv["editora"],
-                     "ano": int(livros_csv["ano"]),
+                     "ano": int(livros_csv["ano"]), # converte o valor para inteiro
                      "situação": livros_csv["situação"]
                  }
-                 biblioteca.append(livro)
+                 biblioteca.append(livro) #adiciona o livro formatado a lista principal
     except FileNotFoundError:
+        #se o arquivo não for encontrado, ele irá informar e retornar a biblioteca vazia
         print("Arquivo CSV não foi encontrado. a biblioteca irá iniciar vazia.")
-    return biblioteca
+    return biblioteca #retorna a lista de livros carregados
 
-#função salvar livros
+#função salvar a lista de livros no arquivo csv, sobrescrevendo o conteudo anterior
 def salvar_livros(biblioteca):
+    #abre o arquivo csv no modo de escrita 
     with open(ARQUIVO_CSV, mode='w', newline='', encoding='utf-8') as arquivo_livros:
+        #define os nomes da colunas que devem ser os mesmos nomes que os nomes das chaves dos dicionarios
         campos_biblioteca = ["id", "titulo", "autores", "editora", "ano", "situação"]
         registro_biblioteca = csv.DictWriter(arquivo_livros, fieldnames=campos_biblioteca)
-        registro_biblioteca.writeheader()
+        registro_biblioteca.writeheader() #escreve a primeira linha com os nomes das coluna
+
+        #escreve cada livro da lista como uma linha do arquivo
         for livro in biblioteca:
-            registro_biblioteca.writerow(livro)
+            registro_biblioteca.writerow(livro) #salava cada dicionario como uma linha do csv
